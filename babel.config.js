20bdd471b0dff5d17ef78d82ebc5d5411387f322
brianmuigai2-stack <brian.muigai2@student.moringaschool.com>
2025-10-21 14:58:40 +0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    ["@babel/preset-react", { runtime: "automatic" }],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  // allow transpile for problematic node_modules packages:
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@adobe/css-tools|another-package-you-need)/)"
   ],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
 };
